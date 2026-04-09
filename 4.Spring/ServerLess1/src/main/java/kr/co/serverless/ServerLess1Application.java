94566e373d19fbf48cca32408afdb5b1a4205d71
tig <leehjpp4@gmail.com>
2026-04-09 18:17:46 +0900

    04/09

---
+++ 4.Spring/ServerLess1/src/main/java/kr/co/serverless/ServerLess1Application.java
@@ -0,0 +1,13 @@
+package kr.co.serverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerLess1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerLess1Application.class, args);
+	}
+
+}
---
+++ 4.Spring/ServerLessTest/src/main/java/kr/co/serverless/ServerLessTestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.serverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerLessTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerLessTestApplication.class, args);
+	}
+
+}
